efe60aa0b943bc8d3b2b38a3fe309305b0520867
adminius <dixiedick2000@gmail.com>
2021-12-26 12:33:33 +0300

    problem

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [arr, setArr] = useState([
+    { ID: 12132, Marker: "Marker1", queue: 1 },
+    { ID: 12133, Marker: "Marker2", queue: 2 },
+    { ID: 12134, Marker: "Marker3", queue: 3 },
+    { ID: 12135, Marker: "Marker4", queue: 4 },
+    { ID: 12136, Marker: "Marker5", queue: 5 },
+  ]);
+
+  const arrSet = () => {
+    setArr([...arr, (arr[2] = { ...arr[2], queue: 6 })]);
+    console.log(arr);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={() => arrSet()}>click me</button>
+    </div>
+  );
+}
+
+export default App;
